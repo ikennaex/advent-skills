da28b973c32e514a2cf07c3abad1658a51d3272a
ikennaex <ikennaexcel@gmail.com>
2025-11-12 10:28:17 +0100

    update: devMode

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -2,4 +2,4 @@ const isDev = false; // set to true for local development
 
 export const baseUrl = isDev
   ? "http://localhost:4000"
-  : "https://adventskills-api.onrender.com";
+  : "https://www.adventskills.com.ng";
